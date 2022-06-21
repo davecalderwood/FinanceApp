8ebbd4eb6e0d0559d1a18d45bce3bd774396473d
Dave Calderwood <davidhcalderwood@gmail.com>
2022-06-21 15:17:15 -0400

    Validators

--- src/UI/Input/Input.js
+++ src/UI/Input/Input.js
@@ -1,24 +1,64 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import { validate } from '../../shared/Utils/validators';
 import classes from './Input.module.scss';
 
+const inputReducer = (state, action) => {
+    switch(action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators)
+            }
+        case 'TOUCH':
+            return {
+                ...state,
+                isTouched: true
+            }
+        default:
+            return state;
+    }
+};
+
 const Input = (props) => {
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: '', 
+        isValid: false, 
+        isTouched: false
+    });
+
+    const changeHandler = event => {
+        dispatch({type: 'CHANGE', val: event.target.value, validators: props.validators});
+    }
+
+    const touchHandler = () => {
+        dispatch({type: 'TOUCH'})
+    }
+
     const element = props.element === 'input' ? 
         <input 
             id={props.id} 
             type={props.type}
-            placeholder={props.placeholder} /> : 
+            placeholder={props.placeholder}
+            onChange={changeHandler}
+            onBlur={touchHandler}
+            value={inputState.value} /> : 
         <textarea 
             id={props.id} 
             rows={props.rows || 3}
             type={props.type}
-            placeholder={props.placeholder} />;
+            placeholder={props.placeholder}
+            onChange={changeHandler}
+            onBlur={touchHandler}
+            value={inputState.value} />;
 
+    const inputValidClass = !inputState.isValid && inputState.isTouched ? classes.formControlInvalid : '';
 
     return (
-        <div className={classes.formControl}>
+        <div className={`${classes.formControl} ${inputValidClass}`}>
             <label htmlFor={props.id}>{props.label}</label>
             {element}
+            {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
         </div>
     );
 }
--- src/expenses/components/AddExpense.js
+++ src/expenses/components/AddExpense.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '../../UI/Button/Button';
 import Input from '../../UI/Input/Input';
 import Modal from '../../UI/Modal/Modal';
-
+import { VALIDATOR_REQUIRE } from '../../shared/Utils/validators';
 import classes from '../styles/AddExpense.module.scss';
 
 const AddExpense = (props) => {
@@ -11,7 +11,13 @@ const AddExpense = (props) => {
             <h1>Add Expense Modal</h1>
 
             <form className={classes.expenseForm}>
-                <Input element="input" type="text" label="Title" placeholder="Placeholder" />
+                <Input 
+                    element="input" 
+                    type="text" 
+                    label="Title" 
+                    placeholder="Placeholder"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please Enter a Valid Title" />
             </form>
 
             <Button onClick={props.onClose}>Close</Button>
