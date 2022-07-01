74a7b920ff2536a4cd75057bf9b0727d61cab44f
dcalderwood <david.calderwood@ideaentity.com>
2022-07-01 08:21:16 -0400

    Dropdown change dashboard filter

--- src/dashboard/components/SortedExpenses.js
+++ src/dashboard/components/SortedExpenses.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MOCK_DATA from '../../MOCK_DATA.json';
 import MonthlyCost from './MonthlyCost';
+import Select from 'react-select';
+
+import classes from '../styles/SortedExpenses.module.scss';
 
 const SortedExpenses = () => {
+    const [selectedOption, setSelectedOption] = useState('2022');
+
+    const dropdownSelectHandler = (selectedOption) => {
+        setSelectedOption(selectedOption);
+    };
     var monthsList = [
-        {label: "Janurary", value: '01', totalExpenses: 0},
-        {label: "February", value: '02', totalExpenses: 0},
-        {label: "March", value: '03', totalExpenses: 0},
-        {label: "April", value: '04', totalExpenses: 0},
-        {label: "May", value: '05', totalExpenses: 0},
-        {label: "June", value: '06', totalExpenses: 0},
-        {label: "July", value: '07', totalExpenses: 0},
-        {label: "August", value: '08', totalExpenses: 0},
-        {label: "September", value: '09', totalExpenses: 0},
-        {label: "October", value: '10', totalExpenses: 0},
-        {label: "November", value: '11', totalExpenses: 0},
-        {label: "December", value: '12', totalExpenses: 0},
+        { label: "Janurary", value: '01', totalExpenses: 0 },
+        { label: "February", value: '02', totalExpenses: 0 },
+        { label: "March", value: '03', totalExpenses: 0 },
+        { label: "April", value: '04', totalExpenses: 0 },
+        { label: "May", value: '05', totalExpenses: 0 },
+        { label: "June", value: '06', totalExpenses: 0 },
+        { label: "July", value: '07', totalExpenses: 0 },
+        { label: "August", value: '08', totalExpenses: 0 },
+        { label: "September", value: '09', totalExpenses: 0 },
+        { label: "October", value: '10', totalExpenses: 0 },
+        { label: "November", value: '11', totalExpenses: 0 },
+        { label: "December", value: '12', totalExpenses: 0 },
     ]
-    
+
     // Filter out expenses
     const expenses = MOCK_DATA.filter(i => i.Category !== 'Savings');
     // Parse data
     var data = JSON.parse(JSON.stringify(expenses));
     // Slice and compare year
-    const dates = data.filter((d) => d.Date.slice(-4) === new Date().getFullYear().toString());
+    const dates = data.filter((d) => d.Date.slice(-4) === selectedOption.value);
 
     dates.forEach(i => {
         var expense = i.Amount;
@@ -49,11 +57,29 @@ const SortedExpenses = () => {
             label={item.label} />
     })
 
+    var options = [...new Set(data.map(item => item.Date.slice(-4)))];
+    var dropdownOptions = [];
+    options.forEach(element => {
+        dropdownOptions.push({ label: element, value: element });
+    });
+    dropdownOptions.sort((a, b) => a.value > b.value ? 1 : -1);
+
     return (
-        <>
-            {monthlyCost}
-        </>
+        <div className={classes.dropdownAndCards}>
+            <div className={classes.dropdown}>
+                <Select
+                    id="categoryDropdown"
+                    options={dropdownOptions}
+                    onChange={dropdownSelectHandler}
+                    defaultValue={selectedOption}
+                    selected={selectedOption}
+                    isSearchable={false}
+                    placeholder="Filter by Year" />
+            </div>
+
+            <div className={classes.cards}>{monthlyCost}</div>
+        </div>
     );
 }
- 
+
 export default SortedExpenses;
