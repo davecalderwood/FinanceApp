390c42a8917dc6b4f44a5536dde2a01d40c3a336
dcalderwood <david.calderwood@ideaentity.com>
2022-08-29 16:07:22 -0400

    localstorage

--- src/Budget/Pages/MonthlyBudget.js
+++ src/Budget/Pages/MonthlyBudget.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import BudgetBuilder from '../Components/BudgetBuilder';
+import localstorage from '../../localstorage.json'
 
 const MonthlyBudget = () => {
+    localStorage.setItem('testObject', JSON.stringify(localstorage));
+    var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
+
+    var test = retrievedObject.map(i => {
+        return i.ItemComponent
+    });
+
     return (
         <>
             <BudgetBuilder />
+            {/* {test} */}
         </>
     );
 }
